12ba2c61e3385a214ab4989a5017f50b00241d19
Toby Liu <toby.liu@hulu.com>
2024-07-15 18:39:05 -0700

    I am poor

---
+++ section3/I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Toby Liu on 7/15/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
