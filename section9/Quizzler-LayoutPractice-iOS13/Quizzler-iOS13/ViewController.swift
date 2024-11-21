162050f81c71b3ab052b211df7797e8b1715f250
Toby Liu <toby.liu@hulu.com>
2024-11-21 14:04:28 -0800

    playing with struct:

--- section9/Quizzler-LayoutPractice-iOS13/Quizzler-iOS13/ViewController.swift
+++ section9/Quizzler-LayoutPractice-iOS13/Quizzler-iOS13/ViewController.swift
@@ -17,9 +17,9 @@ class ViewController: UIViewController {
 
 
     let quiz = [
-        "Four + Two is equal to Six",
-        "Five - Three is greater than One",
-        "Three + Eight is less than Ten"
+        ["Four + Two is equal to Six", "True"],
+        ["Five - Three is greater than One", "True"],
+        ["Three + Eight is less than Ten", "False"]
     ]
     
     var questionNumber = 0
@@ -30,13 +30,28 @@ class ViewController: UIViewController {
         updateUI()
     }
 
-    @IBAction func answerButtonPressed(_ sender: Any) {
-        questionNumber += 1
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        let userAnswer = sender.currentTitle
+        let actualAnswer = quiz[questionNumber][1]
+        
+        if (userAnswer == actualAnswer) {
+            print ("Right")
+        } else {
+            print ("Wrong")
+        }
+        
+        if (questionNumber + 1 < quiz.count) {
+            questionNumber += 1
+        } else {
+            questionNumber = 0
+        }
+        
         updateUI()
+
     }
     
     func updateUI() {
-        questionLabel.text = quiz[questionNumber]
+        questionLabel.text = quiz[questionNumber][0]
     }
     
 }
---
+++ section9/structs.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+struct Town {
+    let name = "Tobyland"
+    var citizens = ["person", "another", "same"]
+    var resources = ["Grain": 100, "Ore": 42, "Wool": 75]
+}
+
+
+
