5388ec4e828578c3e5715ec493f71a775bb2ca1d
Toby Liu <toby.liu@hulu.com>
2024-11-21 15:31:05 -0800

    structs

--- section9/structs.playground/Contents.swift
+++ section9/structs.playground/Contents.swift
@@ -1,10 +1,33 @@
 import UIKit
 
 struct Town {
-    let name = "Tobyland"
-    var citizens = ["person", "another", "same"]
-    var resources = ["Grain": 100, "Ore": 42, "Wool": 75]
+    let name: String
+    var citizens : [String]
+    var resources : [String: Int]
+    
+    init(name: String, citizens: [String], resources: [String: Int]) {
+        self.name = name
+        self.citizens = citizens
+        self.resources = resources
+    }
+    
+    func fortify() {
+        print("Defenses increased")
+    }
 }
 
+var myTown = Town(
+    name: "TobyLand",
+    citizens: ["Keanu", "John"],
+    resources: ["Grain": 100]
+)
 
+print(myTown.citizens)
+print("\(myTown.name) has \(myTown.resources["Grain"] ?? 0) of grain")
 
+myTown.citizens.append("Keanu Reeves")
+print(myTown.citizens.count)
+
+myTown.fortify()
+
+var ghostTown = Town(name: "Ghosty Ghost", citizens: [], resources: ["Tumbleweed": 1])
