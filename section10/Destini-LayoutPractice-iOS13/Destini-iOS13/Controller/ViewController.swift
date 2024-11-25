59ad2c4f96620aa02d0063748253da0966e43fcf
Toby Liu <toby.liu@hulu.com>
2024-11-25 10:20:41 -0800

    Initial alignment of items and hooking up to viewcontroller

--- section10/Destini-LayoutPractice-iOS13/Destini-iOS13/Controller/ViewController.swift
+++ section10/Destini-LayoutPractice-iOS13/Destini-iOS13/Controller/ViewController.swift
@@ -8,15 +8,29 @@
 
 import UIKit
 
+let choice1 = "Take a left."
+let choice2 = "Take a right."
+let story0 = "You see a fork in the road."
+
 class ViewController: UIViewController {
     
+    @IBOutlet weak var choice1Btn: UIButton!
+    @IBOutlet weak var choice2Btn: UIButton!
+    @IBOutlet weak var storyLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         print("loaded")
-
+        
+        storyLabel.text = story0
+        choice1Btn.setTitle(choice1, for: .normal)
+        choice2Btn.setTitle(choice2, for: .normal)
     }
 
-
+    @IBAction func choiceMade(_ sender: UIButton) {
+        print(sender.titleLabel?.text! ?? "unknown button")
+    }
+    
 }
 
