ea84e697b942d85aa46b38872b42f9833aa17cc4
ybot1122 <ybot1122@gmail.com>
2024-12-14 12:10:32 -0800

    get api from xcconfig

--- section14/ByteCoin-iOS13/ByteCoin/Model/CoinManager.swift
+++ section14/ByteCoin-iOS13/ByteCoin/Model/CoinManager.swift
@@ -8,14 +8,16 @@
 
 import Foundation
 
+let apiKey = Bundle.main.object(forInfoDictionaryKey: "Coin_api_key") ?? "bad"
+
 struct CoinManager {
     
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "YOUR_API_KEY_HERE"
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
 
     func getCoinPrice(for currency: String) -> Int{
+        print(apiKey)
         return 100
     }
     
