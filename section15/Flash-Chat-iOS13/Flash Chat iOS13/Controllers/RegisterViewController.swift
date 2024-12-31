b5043da82c386c03416c1dcb0149c89e832a4d08
ybot1122 <ybot1122@gmail.com>
2024-12-31 13:29:08 -0800

    Firebase cocoapods. Register screen.

--- section15/Flash-Chat-iOS13/Flash Chat iOS13/Controllers/RegisterViewController.swift
+++ section15/Flash-Chat-iOS13/Flash Chat iOS13/Controllers/RegisterViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class RegisterViewController: UIViewController {
 
@@ -14,6 +15,22 @@ class RegisterViewController: UIViewController {
     @IBOutlet weak var passwordTextfield: UITextField!
     
     @IBAction func registerPressed(_ sender: UIButton) {
+        
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            Auth.auth().createUser(withEmail: email, password: password) {
+                authResult, error in
+                if let e = error {
+                    print(e)
+                } else {
+                    // Navigate to the ChatViewController
+                    self.performSegue(withIdentifier: "RegisterToChat", sender: self)
+                }
+            }
+        } else {
+            
+            print("invalid inputs. cannot create account")
+        }
+
     }
     
 }
